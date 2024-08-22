c426878fce96fed7b7ee00949dce87d225ad9ac8
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-08-22 13:30:00 +0530

    Second Commit

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ function converter() {
   if (temptype == "celsius") {
     var cel = (temp - 32) * (5 / 9);
     document.getElementById(
-      "converted_to_far"
+      "converted_value"
     ).innerHTML = `Celsius value is ${cel} C.`;
+    document.getElementById("temperature_type").innerHTML = "The given value is taken as Fahrenheit";
   } else if (temptype == "fahrenheit") {
     var far = temp * (9 / 5) + 32;
     document.getElementById(
-      "converted_to_cel"
+      "converted_value"
     ).innerHTML = `Fahrenheit value is ${far} F.`;
+    document.getElementById("temperature_type").innerHTML = "The given value is taken as Celsius";
+
   } else {
     document.getElementById("converted_value").innerHTML = Invalid;
   }
