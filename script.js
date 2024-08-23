af36e15122818a3eb38ad28b20ba543a4a0012ef
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-08-23 10:59:14 +0530

    Third Commit

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ function converter() {
   let temp = document.getElementById("temp").value;
   let temptype = document.getElementById("temptype").value;
   if (temptype == "celsius") {
-    var cel = (temp - 32) * (5 / 9);
+    var cel = temp;
     document.getElementById(
       "converted_value"
     ).innerHTML = `Celsius value is ${cel} C.`;
-    document.getElementById("temperature_type").innerHTML = "The given value is taken as Fahrenheit";
+    // document.getElementById("temperature_type").innerHTML =
+    //   "The given value is taken as Fahrenheit";
   } else if (temptype == "fahrenheit") {
     var far = temp * (9 / 5) + 32;
     document.getElementById(
       "converted_value"
     ).innerHTML = `Fahrenheit value is ${far} F.`;
-    document.getElementById("temperature_type").innerHTML = "The given value is taken as Celsius";
-
+    // document.getElementById("temperature_type").innerHTML =
+    //   "The given value is taken as Celsius";
+  } else if (temptype == "kelvin") {
+    var kel = temp + 273.15;
+    document.getElementById(
+      "converted_value"
+    ).innerHTML = `Kelvin value is ${kel} K.`;
+    // document.getElementById("temperature_type").innerHTML =
+    //   "The given value is taken as Celsius";
   } else {
     document.getElementById("converted_value").innerHTML = Invalid;
   }
